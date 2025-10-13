8541ad6b4f3827140b971ab46c126b8d1187dfb2
acpuchades <59510094+acpuchades@users.noreply.github.com>
2025-10-13 23:47:18 +0200

    Implement demographics output form

--- R/output.r
+++ R/output.r
@@ -70,4 +70,84 @@ output_personalinformation <- consents |>
     )
   )
 
+output_demographics <- consents |>
+  left_join(
+    ufela_pacientes |> select(
+      pid, nhc, sexo, fecha_nacimiento, provincia_nacimiento,
+      codigo_postal, estudios, situacion_laboral_actual,
+      situacion_laboral_actual_otra, exitus, fecha_exitus
+    ),
+    by = "nhc"
+  ) |>
+  left_join(ufela_clinica |> select(pid, fumador, starts_with("historia_familiar_")), by = "pid") |>
+  transmute(
+    family_history_dic = if_else(
+      historia_familiar_alzheimer | historia_familiar_parkinson | historia_familiar_motoneurona,
+      1, 0
+    ),
+    als_fam_hist_type = if_else(historia_familiar_motoneurona, 1, 2),
+    adopted = 98,
+    father_alive = NA,
+    father_death_cause = NA,
+    father_death_age = NA, # text?
+    mother_alive = NA,
+    mother_death_cause = NA,
+    mother_death_age = NA, # text?
+    fam_tree = 0,
+    adv_fam = 0,
+    sex = sexo |> case_match("Hombre" ~ 1, "Mujer" ~ 2, .default = 98),
+    dob = fecha_nacimiento,
+    cp_current = codigo_postal,
+    res_type_current = NA,
+    cp_life = NA,
+    res_type_life = NA,
+    country_origin = NA,
+    province_origin = provincia_nacimiento,
+    ehnicity_2_yn = NA,
+    consanguinity = 98,
+    consanguinity_type = NA,
+    dominance = NA,
+    smoke = case_match(fumador, 
+      "No fumador" ~ 0,
+      "Exfumador" ~ 1,
+      "Fumador" ~ 2,
+    ),
+    start_smoke_age = NA,
+    cigar_smoked = NA,
+    end_smoke_age = NA,
+    alcohol = NA,
+    alcohol_freq = NA,
+    alcohol_units = NA,
+    start_alcohol_age = NA,
+    end_alcohol_age = NA,
+    adv_risk = 0,
+    adv_med_history = 0,
+    educ_level = estudios |> case_match(
+      "No sabe leer ni escribir" ~ 0, # ISCED 0
+      "Primarios incompletos" ~ 0, # ISCED 0
+      "Primarios completos" ~ 1, # ISCED 1
+      "Secundarios (ESO, BUP, COU, etc)" ~ 2, # ISCED 2
+      "FP (grado medio o superior)" ~ 4, # ISCED 4-5
+      "Universidad" ~ 6, # ISCED 6-7
+      "Doctorado" ~ 8, # ISCED 8
+      "Otro" ~ NA,
+    ),
+    educ_years = NA,
+    employ_premorbid = NA,
+    net_income_bef_onset = NA,
+    income_periods = NA,
+    employ_current = situacion_laboral_actual |> case_match(
+      "Trabaja" ~ 1,
+      "Parado" ~ 3,
+      "Parado con subsidio | Prestación" ~ 3,
+      "Labores de la casa" ~ 5,
+      "Jubilado" ~ 6,
+      "Incapacitado (o con invalidez permanente)" ~ 7,
+      "Otra" ~ 99
+    ),
+    employ_current_other = situacion_laboral_actual_otra |> na_if(""),
+    incapacity = coalesce(if_else(situacion_laboral_actual == 7, NA, 0), 98),
+    incapacity_type = 98,
+    disability = coalesce(if_else(situacion_laboral_actual == 7, NA, 0), 98),
+    previous_jobs = 0,
   )
