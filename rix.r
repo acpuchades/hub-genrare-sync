324d89ef3de1dfd9b5561085c4718cdd7468a6f8
acpuchades <59510094+acpuchades@users.noreply.github.com>
2025-10-13 14:33:43 +0200

    Initial commit

---
+++ rix.r
@@ -0,0 +1,15 @@
+rix::rix(
+         project_path = getwd(),
+         ide = "positron",
+         r_ver = "4.5.1",
+         r_pkgs = c(
+            "DBI",
+            "here",
+            "janitor",
+            "readxl",
+            "rix",
+            "RSQLite",
+            "tidyverse"
+         ),
+         overwrite = TRUE
+)
